55d62e95aac0de352ebbb722c1a1f3ea443769e2
Sebe Keller <sebethkeller@gmail.com>
2017-04-03 09:30:18 -0400

    Add basic notes for keyboard press

--- noise.js
+++ noise.js
@@ -14,12 +14,35 @@ oscillator.connect(soundMaker.destination);
 //Add event listeners
 function addListen(){
 	var noiseBox = document.getElementById("noiseBox");
+	//Allow div to be clickable
+	noiseBox.setAttribute("tabindex",0)
+	//For use like a theramin
 	noiseBox.addEventListener("mousemove", adjustFrequency);
+	//For use like a keyboard
+	noiseBox.addEventListener("keydown", keyboardFrequency);
 }
 
 //Adjust the frequency of the oscillator based on the mouse's position on the screen
 function adjustFrequency(e){
 	oscillator.frequency.value = e.clientY;
+}
+
+//Adjust frequency with keyboard press
+function keyboardFrequency(e){
+	if(e.key == "z"){
+		//c4 note
+		oscillator.frequency.value = 261.63;
+	} else if (e.key == "x"){
+		//d4 note
+		oscillator.frequency.value = 293.66;
+	} else if (e.key == "c"){
+		//e4 note
+		oscillator.frequency.value = 329.63;
+	} else if (e.key == "v"){
+		//f4 note
+		oscillator.frequency.value = 349.23;
+	}
+
 }
 
 //Start the oscillator
